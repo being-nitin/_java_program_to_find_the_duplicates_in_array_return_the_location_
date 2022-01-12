f0390050e0e077a78b36aa6f6bcb446fd4b4ed8b
Nitin <nsinghboss02@gmail.com>
2022-01-13 00:26:55 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,26 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Write a program to find whether the array of integers contains a duplicate number. if yes
+	print the location where duplicate numbers are found.
+
+	 */
+        int[] arr = {7,9,8,4,8};
+        System.out.println(Arrays.toString(duplicate(arr)));
+    }
+        public static int[] duplicate(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j< arr.length;j++){
+            if(arr[i]==arr[j]){
+                System.out.println("The duplicate element is "+arr[i]);
+                return new int[]{i,j};
+            }
+        }
+        }
+        return new int[]{0,0};
+        }
+    }
+
